e819ca8b37b64e33c387d5a17516549cd1da65f8
Ashleigh Crosby <ashleigh.crosby@vitrifi.net>
2025-04-15 13:00:36 +0100

    allow for ipv6 addresses to be read from VMs

--- cloudstack/data_source_cloudstack_instance.go
+++ cloudstack/data_source_cloudstack_instance.go
@@ -161,7 +161,7 @@ func instanceDescriptionAttributes(d *schema.ResourceData, instance *cloudstack.
 	d.Set("zone_id", instance.Zoneid)
 	d.Set("nic", []interface{}{map[string]string{
 		"ip_address":  instance.Nic[0].Ipaddress,
-		"ip6_address": instance.Nic[0].Ipaddress,
+		"ip6_address": instance.Nic[0].Ip6address,
 	}})
 
 	d.Set("tags", tagsToMap(instance.Tags))
